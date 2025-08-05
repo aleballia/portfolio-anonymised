d31048b78bc9cb591b23a2b8032d96fbc1cd67ca
aleballia <alessandraballiana@gmail.com>
2025-08-05 17:06:34 +0200

    added google tag

--- portfolio-app/src/app/layout.tsx
+++ portfolio-app/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { ThemeProvider } from "./components/ThemeProvider";
 
@@ -25,6 +26,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-B4HB7EX0E8"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-B4HB7EX0E8');
+          `}
+        </Script>
+      </head>
       <body
         className={`${inter.variable} antialiased`}
       >
